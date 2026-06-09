6a1cde9c2ae25fffc04f2dd22431df890688f42a
Najad-Nabil <najadnabil2612@gmail.com>
2026-06-09 18:53:13 +0000

    Minor frontend bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 function App() {
