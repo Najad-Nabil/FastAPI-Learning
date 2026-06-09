30550cc503c69c8e67bef97ca21f933a59752527
Najad-Nabil <najadnabil2612@gmail.com>
2026-06-09 11:19:16 +0000

    Database initialization

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from models import Product
+from config import SessionLocal, engine
+import database_schema
 
 app = FastAPI()
+
+database_schema.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def greet():
@@ -15,8 +19,19 @@ products = [
     Product(id = 4 ,name = "Mouse" ,description =  "Corsair" ,price =  5999 ,quantity =  10)
 ]
 
+def init_db():
+    db = SessionLocal()
+    count = db.query(database_schema.ProductDB).count
+    if count == 0:
+        for product in products:
+            db.add(database_schema.ProductDB(**product.model_dump()))
+        db.commit()
+init_db()
+
 @app.get("/product")
 def view_products():
+    # db = SessionLocal()
+    # db.query()
     return products
 
 @app.get("/product/{product_id}")
